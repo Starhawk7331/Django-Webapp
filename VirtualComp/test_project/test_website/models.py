5879c320c7051b08e8052472b7aa01cab1ec9b20
Daniel Kaiser <40146720220195@litec.ac.at>
2023-09-30 16:34:39 +0200

    doc: added login form testing

--- VirtualComp/test_project/test_website/models.py
+++ VirtualComp/test_project/test_website/models.py
@@ -4,6 +4,8 @@ class Family_members(models.Model):
     firstname = models.CharField(max_length=255)
     secondname = models.CharField(max_length = 255)
     lastname = models.CharField(max_length = 255)
+    username = models.CharField(null=True)
+    password = models.CharField(null=True)
 
     def __str__(self):
         return f"{self.firstname} {self.secondname} {self.lastname}"
--- VirtualComp/test_project/test_website/views.py
+++ VirtualComp/test_project/test_website/views.py
@@ -1,6 +1,29 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Family_members
 
 def home(request):
-    template = loader.get_template("home.html")
+    if request.method == 'GET':
+        username = request.GET.get('username', None)
+        password = request.GET.get('password', None)
+
+        
+        
+        if username in Family_members.objects.all() and password in Family_members.objects.all():
+            template = loader.get_template("home.html")
+            return HttpResponse(template.render())
+        
+        else:
+            return HttpResponse("debug1")
+            #template = loader.get_template("login.html")
+            #return HttpResponse(template.render()) 
+    else:
+        return HttpResponse("debug2")
+        #template = loader.get_template("login.html")
+        #return HttpResponse(template.render())
+    
+    
+
+def login(request):
+    template = loader.get_template("login.html")
     return HttpResponse(template.render())

c3e2918acb0ea7d64276135596d4cca60ff25c39
Daniel Kaiser <40146720220195@litec.ac.at>
2023-10-08 13:33:10 +0200

    doc: mad static root

--- VirtualComp/test_project/test_project/settings.py
+++ VirtualComp/test_project/test_project/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles'
 ]
 
+STATIC_ROOT = BASE_DIR / "staticRoot"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

a15713aecdbb192ef0cc17cd80e71b7a1a3d9b7b
Daniel Kaiser <40146720220195@litec.ac.at>
2023-10-08 14:36:47 +0200

    doc: moved templates and static to app

--- VirtualComp/test_project/test_website/urls.py
+++ VirtualComp/test_project/test_website/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.views.generic.base import TemplateView
+from django.urls import path,include
 from . import views
 
+
 urlpatterns =[
-    path('home/',views.home, name='home'),
-    path('',views.login, name='login'),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"), 
 ]

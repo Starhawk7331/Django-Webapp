72eaf770e430933a36387f8c1f0f3f2d57d667bc
Daniel Kaiser <40146720220195@litec.ac.at>
2023-10-08 12:02:24 +0200

    doc: just saving

--- VirtualComp/test_project/test_website/views.py
+++ VirtualComp/test_project/test_website/views.py
@@ -1,31 +1,21 @@
-from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.template import loader
-from .models import Family_members
 
-@csrf_protect
+
 def home(request):
-
-    if request.method == 'post':
-
-        Username = request.post.get('username', None)
-        Password = request.post.get('password', None)
-
-        user = Family_members.objects.get(username=Username)
-
-        if user.password == Password:
-            
-            template = loader.get_template("home.html")
-            return HttpResponse(template.render())
-        else:
-            template = loader.get_template("login.html")
-            return HttpResponse(template.render()) 
-    else: 
-        template = loader.get_template("login.html")
+    Username = request.post.get('username', None)
+    Password = request.post.get('password', None)
+    user = authenticate(Username,Password)
+    if user is not None:
+        template = loader.get_template("home.html")
         return HttpResponse(template.render())
+    else:
+        login(request)
+
     
     
-@csrf_protect
+
 def login(request):
     template = loader.get_template("login.html")
     return HttpResponse(template.render())
